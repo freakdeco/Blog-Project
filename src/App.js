f883b92caf6f477f5672787c0383fa0bc1d1469f
Ali <96638533+freakdeco@users.noreply.github.com>
2024-02-15 06:53:30 +0000

    Student Card

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/card';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className="App">
+      <h1>Card</h1>
+      <Card />
+    </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
