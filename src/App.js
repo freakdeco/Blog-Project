cad48a12ad17d40f5931e88a462c99984ba7b6a3
Ali <96638533+freakdeco@users.noreply.github.com>
2024-03-07 05:55:14 +0000

    Temp 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from './components/card';
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
 
 function App() {
   return (
-    <div className = "App">
-      <h1 className = 'text-red-500 text-5xl'> Hello World!</h1>
-
+    <div className = "App font-opensans">
+      <HomePage/>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { images } from "../constants/";
+
+
+
+const navItemsInfo = [
+  {name: "Home"},
+  {name: "Articles"},
+  {name: "Pages"},
+  {name: "Pricing"},
+  {name: "Faq"}
+]
+
+
+const NavItem = ({name}) => {
+  return (
+    <li className="relative group">
+      <a href="/" className="px-4 py-2">
+        {name}
+      </a>
+      <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0  group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+        /
+      </span>
+
+    </li>
+  )
+}
+
+const Header = () => {
+  const [navIsVisible, setNavIsVisible] = useState(false);
+
+  const navVisibilityHandler = () => {
+    setNavIsVisible((curState) => {
+      return !curState;
+    });
+  };
+
+  return (
+    <section>
+        <header className="container mx-auto px-5 flex justify-between py-4 items-center">
+            <div>
+              <img  className="w-16" src={images.Logo} alt="logo" />
+            </div>
+            <div className= "lg:hidden z-50">
+              {navIsVisible ? (
+              <AiOutlineClose className="w-6 h-6" onClick={navVisibilityHandler}/> 
+              ) : (
+              <AiOutlineMenu className="w-6 h-6" onClick={navVisibilityHandler}/>
+              )} 
+              </div>
+
+            <div className={`${navIsVisible ? "right-0" : "-right-full" 
+            } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center
+            `}>
+
+
+              <ul className="text-white lg:text-dark-soft flex flex-col lg:flex-rowgap-x-2 font-semibold">
+                {navItemsInfo.map((item) => (
+                  <NavItem key={item.name}  name={item.name} />
+
+                ))}
+              </ul>
+
+              <button className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"> Sign In</button>
+            </div>
+        </header>
+    </section>
+  )
+}
+
+export default Header
